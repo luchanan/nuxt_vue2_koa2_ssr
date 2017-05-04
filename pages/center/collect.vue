bd79ce3841abb212764e9833b7595b6450771611
luchanan <398778994@qq.com>
2017-05-04 23:39:06 +0800

    'init’

---
+++ pages/center/collect.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    收藏
+  </section>
+</template>
+<script>
+</script>
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
---
+++ pages/center/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <ul>
+      <li v-for="item in firms">
+        {{item.name}}
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+import axios from 'axios'
+export default{
+  async asyncData({}){
+    let { data } = await axios.get('http://m.maizuo.com/v4/api/billboard/home?__t='+(new Date())*1)
+    return {firms:data.data.billboards}
+  },
+  head(){
+    return {
+      title:this.firms[0].name
+    }
+  }
+}
+</script>
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
